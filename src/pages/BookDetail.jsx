09aca12ec0285b5fda94f77aeef65a4196d8f818
Rabab Makki <rabab.m.alaman@gmail.com>
2025-05-02 23:50:23 +0300

    Add the detail of books to view in the page

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -20,6 +20,13 @@ function BookDetail() {
   return (
     <div>
       <h2>Book Detail:</h2>
+      <p>{book.book_pic_url}</p>
+      <p>{book.book_title}</p>
+      <p>{book.book_auther}</p>
+      <p>{book.book_year}</p>
+      <p>{book.book_no_of_pages}</p>
+      <p>{book.book_language}</p>
+      <p>{book.book_brief}</p>
     </div>
   )
 }
