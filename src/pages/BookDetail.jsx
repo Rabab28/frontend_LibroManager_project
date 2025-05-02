44ab1acf9e061a57a63e0f0697a1c7a0b6fbc994
Rabab Makki <rabab.m.alaman@gmail.com>
2025-05-02 23:47:39 +0300

    Add a function to get the book from API and add state

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 
 function BookDetail() {
-    
+    const {id} = useParams()
+    const {book , setBook} = useState(null)
+
+    async function getSingleBook() {
+        const responce = await axios.get(`http://127.0.0.1:8000/api/books/${id}`)
+        setBook(responce.data)
+    }
+
+    useEffect(() =>{ 
+        getSingleBook()
+        console.log(id)
+    }, []) // empty array to stop loading for an infinite in the consoler
+    if(!book) return <h1>Loading your Book...</h1>
+
   return (
     <div>
       <h2>Book Detail:</h2>
