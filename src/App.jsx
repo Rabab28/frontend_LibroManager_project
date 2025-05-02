fcb6ebd0a160e78a9577f84746cd60af1269935f
Rabab Makki <rabab.m.alaman@gmail.com>
2025-05-02 23:34:00 +0300

    Fix the code to list the books

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router'
 import React from "react";
 
 import My_library from "./pages/My_library";
-import BookAdd from "./pages/BookAdd";
+// import BookAdd from "./pages/BookAdd";
 
 function App() {
   return(   
     <Router>
       <Routes>
         <Route path='/' element={<My_library />}/>
-        <Route path='/books/new' element={<BookAdd />} />
-      </Routes>
-    </Router>
+        {/* <Route path='/books/new' element={<BookAdd />} /> */}
+     </Routes>
+    </Router> 
   )
 }
 
--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,42 +1,44 @@
-import axios from 'axios'
 import React from 'react'
+import axios from 'axios'
+
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router'
+// import { Link } from 'react-router'
 
 function BookList() {
 
     const [books, setBooks] = useState([])
 
     async function getAllBooks() {
-        try {
-            const responce = await axios.get('http"//127.0.0.1:8000/api/books')
-            console.log(responce)
-            setBooks(responce.data)
-        } catch (err){
-            console.error('Error getting books:', err)
+        try{
+            const response = await axios.get('http://127.0.0.1:8000/api/books/')
+            console.log(response.data)
+            setBooks(response.data)  
+        } catch (err) {
+            console.error('Error fetching posts:', err)
         }
+        
     }
+
     useEffect(()=> {
-        getAllBooks()
-    }, [])
+        getAllBooks() }, [])
     
 
-  return (
-    <div>
-        <h2>My Library:</h2>
-        <ul>
-            {books.map(book => {
-                return (
-                    <li key={book.book_id}>
-                        <p>{book.book_pic_url}</p>
-                        <p>{book.book_title}</p>
-                        <p>{book.book_author}</p>
-                        {/* <Link to={`/books/${book.id}`}>{book.title}</Link> */}
-                    </li>
-                )
-            })}
-        </ul>
-    </div>
+    return (
+        <div>
+            <h2>My Library:</h2>
+            <ul>
+                {books.map(book => {
+                    return(
+                        <li key={book.id}>
+                            <p><img src={book.book_pic_url}/></p>
+                            <p>{book.book_title}</p>
+                            <p>{book.book_author}</p>
+                            {/* <Link to={`/books/${book.id}`}>{book.title}</Link> */}
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
   )
 }
 
